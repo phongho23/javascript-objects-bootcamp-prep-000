285c2721e70e5f7085526c4f363b095c4127da29
Phong Ho <ho.phongt@gmail.com>
2018-03-06 04:19:31 +0000

    Old code that is not working

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({
+  Nipsey: "Check It Out",
+  Hamilton: "Charlene",
+  Hailey: "Starving"
+});
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist.artistName = songTitle
+  return playlist
+}
+
